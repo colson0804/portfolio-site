c04aa6d886aa820535397ee86b5dcbf002d5cc7c
Craig Olson <craig.olson804@gmail.com>
2023-02-13 11:59:17 -0800

    index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in Seattle!
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Craig Olson
+          </Heading>
+          <p>Digital Craftsman ( Developer / Musician / Producer / Artist )</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
