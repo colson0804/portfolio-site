abeda0dd02ecb8038adf1388e8c598554b4651d2
Craig Olson <craig.olson804@gmail.com>
2023-02-14 11:24:28 -0800

    Add a dummy transition

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {
   Image,
   useColorModeValue
 } from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
   return (
@@ -43,6 +44,12 @@ const Page = () => {
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
