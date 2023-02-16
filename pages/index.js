3d4f442c62ba740d5274225ee058fc078fd51876
Craig Olson <craig.olson804@gmail.com>
2023-02-16 12:30:42 -0800

    Implement page animations

--- pages/index.js
+++ pages/index.js
@@ -11,92 +11,95 @@ import Paragraph from '../components/paragraph'
 import NextLink from 'next/link'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Seattle!
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Craig Olson
-          </Heading>
-          <p>Digital Craftsman ( Developer / Musician / Producer )</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/craig.jpg"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a full-stack developer based in Seattle!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Craig is a full stack developer with a passion for making things,
-          whether that be digital services, music, or art. In his free time, he
-          can be found hanging out with his guitar and synthesizers. Currently
-          he has a project called{' '}
-          <NextLink href="/works/inkdrop">Bicycle Thieves</NextLink>
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Craig Olson
+            </Heading>
+            <p>Digital Craftsman ( Developer / Musician / Producer )</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/craig.jpg"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1993</BioYear>
-          Born in Evanston, IL
-        </BioSection>
-        <BioSection>
-          <BioYear>2015</BioYear>
-          Completed a Bachelor of Arts in Computer Science at Northwestern
-          University
-        </BioSection>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I love
-        </Heading>
-        <Paragraph>
-          Music, Cooking, Photography, Guitar, Piano, Drums, Synthesizers
-        </Paragraph>
-      </Section>
-      <Section delay={0.4}>
-        <Heading as="h3" variant="section-title">
-          Things I'm Learning
-        </Heading>
-        <Paragraph>How to bullet point my bio page</Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Craig is a full stack developer with a passion for making things,
+            whether that be digital services, music, or art. In his free time,
+            he can be found hanging out with his guitar and synthesizers.
+            Currently he has a project called{' '}
+            <NextLink href="/works/inkdrop">Bicycle Thieves</NextLink>
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1993</BioYear>
+            Born in Evanston, IL
+          </BioSection>
+          <BioSection>
+            <BioYear>2015</BioYear>
+            Completed a Bachelor of Arts in Computer Science at Northwestern
+            University
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I love
+          </Heading>
+          <Paragraph>
+            Music, Cooking, Photography, Guitar, Piano, Drums, Synthesizers
+          </Paragraph>
+        </Section>
+        <Section delay={0.4}>
+          <Heading as="h3" variant="section-title">
+            Things I'm Learning
+          </Heading>
+          <Paragraph>How to bullet point my bio page</Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,26 +1,26 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 // import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        {/* <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem
-            id="inkdrop"
-            title="Inkdrop" /*thumbnail={thumbInkdrop}*/
-          >
+          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
             A markdown note taking app
           </WorkGridItem>
         </Section>
-      </SimpleGrid>
-    </Container>
+      </SimpleGrid> */}
+      </Container>
+    </Layout>
   )
 }
 
